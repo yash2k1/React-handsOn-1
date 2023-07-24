379b86e3312c52f8fd612a5b41608b5d87533dc8
yash gupta <yashgupta1mole@gmail.com>
2023-07-24 14:35:44 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import { useState } from "react";
 import './App.css'
-import FunctionalComponent from './component/FuncComponent';
-import ClassComponent from './component/ClassComponent';
-import ClassCall from "./ClassCall";
 import FuncCompo from "./component/FuncCompo";
 import ClassCompo from "./component/ClassCompo";
 function App() {
-  const [state,setState]=useState(false);
-  const [ClassState,ClassSetState]=useState(false);
-  return (
-  //   <div className="container">
-  //     <div className="main">
-  //   <h1 >Styling using function and class component</h1>
-  //   <button className="btn1" onClick={()=>{setState(!state)}}>To see styling in function components</button>
-    // <button className="btn1" onClick={()=>{ClassSetState(!ClassState)}}>To see styling in class components</button>
-  // </div>  
 
-  //   <div className="header">
-  //     <div className="func">
-  //     {state && <FunctionalComponent />}
-  //     </div>
-  //     <div className="cls">
-  //     {ClassState && <ClassComponent />}
-     
-  //     </div>
-  //   </div>
- 
-    
-  //   </div>
+  return (
   <>
    <h1 >Styling using function and class component</h1>
-  
-  
- 
   <div className="container">
-
-
   <div className="func">
   <FuncCompo/>
 
